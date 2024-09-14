f9c732b4e7060020918dd77f62d2e834801acbf1
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-14 14:27:23 +0700

    added:fake data for dev env

---
+++ data/fake data.sql
@@ -0,0 +1,201 @@
+INSERT INTO provinces(code,name,name_en,full_name,full_name_en,code_name) VALUES
+('01','Hà Nội','Ha Noi','Thành phố Hà Nội','Ha Noi City','ha_noi'),
+('02','Hà Giang','Ha Giang','Tỉnh Hà Giang','Ha Giang Province','ha_giang'),
+('04','Cao Bằng','Cao Bang','Tỉnh Cao Bằng','Cao Bang Province','cao_bang'),
+('06','Bắc Kạn','Bac Kan','Tỉnh Bắc Kạn','Bac Kan Province','bac_kan'),
+('08','Tuyên Quang','Tuyen Quang','Tỉnh Tuyên Quang','Tuyen Quang Province','tuyen_quang'),
+('10','Lào Cai','Lao Cai','Tỉnh Lào Cai','Lao Cai Province','lao_cai'),
+('11','Điện Biên','Dien Bien','Tỉnh Điện Biên','Dien Bien Province','dien_bien'),
+('12','Lai Châu','Lai Chau','Tỉnh Lai Châu','Lai Chau Province','lai_chau'),
+('14','Sơn La','Son La','Tỉnh Sơn La','Son La Province','son_la'),
+('15','Yên Bái','Yen Bai','Tỉnh Yên Bái','Yen Bai Province','yen_bai'),
+('17','Hoà Bình','Hoa Binh','Tỉnh Hoà Bình','Hoa Binh Province','hoa_binh'),
+('19','Thái Nguyên','Thai Nguyen','Tỉnh Thái Nguyên','Thai Nguyen Province','thai_nguyen'),
+('20','Lạng Sơn','Lang Son','Tỉnh Lạng Sơn','Lang Son Province','lang_son'),
+('22','Quảng Ninh','Quang Ninh','Tỉnh Quảng Ninh','Quang Ninh Province','quang_ninh'),
+('24','Bắc Giang','Bac Giang','Tỉnh Bắc Giang','Bac Giang Province','bac_giang'),
+('25','Phú Thọ','Phu Tho','Tỉnh Phú Thọ','Phu Tho Province','phu_tho'),
+('26','Vĩnh Phúc','Vinh Phuc','Tỉnh Vĩnh Phúc','Vinh Phuc Province','vinh_phuc'),
+('27','Bắc Ninh','Bac Ninh','Tỉnh Bắc Ninh','Bac Ninh Province','bac_ninh'),
+('30','Hải Dương','Hai Duong','Tỉnh Hải Dương','Hai Duong Province','hai_duong'),
+('31','Hải Phòng','Hai Phong','Thành phố Hải Phòng','Hai Phong City','hai_phong'),
+('33','Hưng Yên','Hung Yen','Tỉnh Hưng Yên','Hung Yen Province','hung_yen'),
+('34','Thái Bình','Thai Binh','Tỉnh Thái Bình','Thai Binh Province','thai_binh'),
+('35','Hà Nam','Ha Nam','Tỉnh Hà Nam','Ha Nam Province','ha_nam'),
+('36','Nam Định','Nam Dinh','Tỉnh Nam Định','Nam Dinh Province','nam_dinh'),
+('37','Ninh Bình','Ninh Binh','Tỉnh Ninh Bình','Ninh Binh Province','ninh_binh'),
+('38','Thanh Hóa','Thanh Hoa','Tỉnh Thanh Hóa','Thanh Hoa Province','thanh_hoa'),
+('40','Nghệ An','Nghe An','Tỉnh Nghệ An','Nghe An Province','nghe_an'),
+('42','Hà Tĩnh','Ha Tinh','Tỉnh Hà Tĩnh','Ha Tinh Province','ha_tinh'),
+('44','Quảng Bình','Quang Binh','Tỉnh Quảng Bình','Quang Binh Province','quang_binh'),
+('45','Quảng Trị','Quang Tri','Tỉnh Quảng Trị','Quang Tri Province','quang_tri'),
+('46','Thừa Thiên Huế','Thua Thien Hue','Tỉnh Thừa Thiên Huế','Thua Thien Hue Province','thua_thien_hue'),
+('48','Đà Nẵng','Da Nang','Thành phố Đà Nẵng','Da Nang City','da_nang'),
+('49','Quảng Nam','Quang Nam','Tỉnh Quảng Nam','Quang Nam Province','quang_nam'),
+('51','Quảng Ngãi','Quang Ngai','Tỉnh Quảng Ngãi','Quang Ngai Province','quang_ngai'),
+('52','Bình Định','Binh Dinh','Tỉnh Bình Định','Binh Dinh Province','binh_dinh'),
+('54','Phú Yên','Phu Yen','Tỉnh Phú Yên','Phu Yen Province','phu_yen'),
+('56','Khánh Hòa','Khanh Hoa','Tỉnh Khánh Hòa','Khanh Hoa Province','khanh_hoa'),
+('58','Ninh Thuận','Ninh Thuan','Tỉnh Ninh Thuận','Ninh Thuan Province','ninh_thuan'),
+('60','Bình Thuận','Binh Thuan','Tỉnh Bình Thuận','Binh Thuan Province','binh_thuan'),
+('62','Kon Tum','Kon Tum','Tỉnh Kon Tum','Kon Tum Province','kon_tum'),
+('64','Gia Lai','Gia Lai','Tỉnh Gia Lai','Gia Lai Province','gia_lai'),
+('66','Đắk Lắk','Dak Lak','Tỉnh Đắk Lắk','Dak Lak Province','dak_lak'),
+('67','Đắk Nông','Dak Nong','Tỉnh Đắk Nông','Dak Nong Province','dak_nong'),
+('68','Lâm Đồng','Lam Dong','Tỉnh Lâm Đồng','Lam Dong Province','lam_dong'),
+('70','Bình Phước','Binh Phuoc','Tỉnh Bình Phước','Binh Phuoc Province','binh_phuoc'),
+('72','Tây Ninh','Tay Ninh','Tỉnh Tây Ninh','Tay Ninh Province','tay_ninh'),
+('74','Bình Dương','Binh Duong','Tỉnh Bình Dương','Binh Duong Province','binh_duong'),
+('75','Đồng Nai','Dong Nai','Tỉnh Đồng Nai','Dong Nai Province','dong_nai'),
+('77','Bà Rịa - Vũng Tàu','Ba Ria - Vung Tau','Tỉnh Bà Rịa - Vũng Tàu','Ba Ria - Vung Tau Province','ba_ria_vung_tau'),
+('79','Hồ Chí Minh','Ho Chi Minh','Thành phố Hồ Chí Minh','Ho Chi Minh City','ho_chi_minh'),
+('80','Long An','Long An','Tỉnh Long An','Long An Province','long_an'),
+('82','Tiền Giang','Tien Giang','Tỉnh Tiền Giang','Tien Giang Province','tien_giang'),
+('83','Bến Tre','Ben Tre','Tỉnh Bến Tre','Ben Tre Province','ben_tre'),
+('84','Trà Vinh','Tra Vinh','Tỉnh Trà Vinh','Tra Vinh Province','tra_vinh'),
+('86','Vĩnh Long','Vinh Long','Tỉnh Vĩnh Long','Vinh Long Province','vinh_long'),
+('87','Đồng Tháp','Dong Thap','Tỉnh Đồng Tháp','Dong Thap Province','dong_thap'),
+('89','An Giang','An Giang','Tỉnh An Giang','An Giang Province','an_giang'),
+('91','Kiên Giang','Kien Giang','Tỉnh Kiên Giang','Kien Giang Province','kien_giang'),
+('92','Cần Thơ','Can Tho','Thành phố Cần Thơ','Can Tho City','can_tho'),
+('93','Hậu Giang','Hau Giang','Tỉnh Hậu Giang','Hau Giang Province','hau_giang'),
+('94','Sóc Trăng','Soc Trang','Tỉnh Sóc Trăng','Soc Trang Province','soc_trang'),
+('95','Bạc Liêu','Bac Lieu','Tỉnh Bạc Liêu','Bac Lieu Province','bac_lieu'),
+('96','Cà Mau','Ca Mau','Tỉnh Cà Mau','Ca Mau Province','ca_mau');
+
+INSERT INTO passenger_types (passenger_type, discount_percentage) 
+VALUES ('Người lớn', 0.00), 
+       ('Trẻ em', 25.00), 
+       ('Sinh viên',15.00), 
+       ('Người cao tuổi', 20.00);
+
+INSERT INTO trains (train_number, train_type) 
+VALUES ('SE1', 'Tàu tốc hành Hà Nội - Sài Gòn'),
+('SE2', 'Tàu tốc hành Sài Gòn - Hà Nội'),
+('SE3', 'Tàu tốc hành Hà Nội - Sài Gòn'),
+('SE4', 'Tàu tốc hành Sài Gòn - Hà Nội'),
+('SE5', 'Tàu tốc hành Hà Nội - Sài Gòn'),
+('SE6', 'Tàu tốc hành Sài Gòn - Hà Nội'),
+('SE7', 'Tàu tốc hành Hà Nội - Sài Gòn'),
+('SE8', 'Tàu tốc hành Sài Gòn - Hà Nội'),
+('SE9', 'Tàu thường Hà Nội - Sài Gòn'),
+('SE10', 'Tàu thường Sài Gòn - Hà Nội'),
+('SE11', 'Tàu thường Hà Nội - Sài Gòn'),
+('SE12', 'Tàu thường Sài Gòn - Hà Nội'),
+('TN1', 'Tàu Thống Nhất chậm Hà Nội - Sài Gòn'),
+('TN2', 'Tàu Thống Nhất chậm Sài Gòn - Hà Nội'),
+('SNT1', 'Tàu Sài Gòn - Nha Trang'),
+('SNT2', 'Tàu Nha Trang - Sài Gòn'),
+('SQN1', 'Tàu Sài Gòn - Quy Nhơn'),
+('SQN2', 'Tàu Quy Nhơn - Sài Gòn'),
+('SPT1', 'Tàu Sài Gòn - Phan Thiết'),
+('SPT2', 'Tàu Phan Thiết - Sài Gòn'),
+('SP1', 'Tàu Hà Nội - Lào Cai'),
+('SP2', 'Tàu Lào Cai - Hà Nội'),
+('NA1', 'Tàu Hà Nội - Vinh'),
+('NA2', 'Tàu Vinh - Hà Nội'),
+('HP1', 'Tàu Hà Nội - Hải Phòng'),
+('HP2', 'Tàu Hải Phòng - Hà Nội');
+
+INSERT INTO stations (station_name, province_code, latitude, longitude) VALUES
+-- Hà Nội
+('Ga Hà Nội', '01', 21.0285, 105.8542),
+('Ga Long Biên', '01', 21.0409, 105.8570),
+
+-- Hải Phòng
+('Ga Hải Phòng', '31', 20.8547, 106.6833),
+
+-- Lào Cai
+('Ga Lào Cai', '10', 22.4842, 103.9707),
+
+-- Yên Bái
+('Ga Yên Bái', '15', 21.7229, 104.9110),
+
+-- Nam Định
+('Ga Nam Định', '36', 20.4275, 106.1681),
+
+-- Ninh Bình
+('Ga Ninh Bình', '37', 20.2456, 105.9746),
+
+-- Thanh Hóa
+('Ga Thanh Hóa', '38', 19.8055, 105.7746),
+
+-- Nghệ An
+('Ga Vinh', '40', 18.6764, 105.6813),
+
+-- Hà Tĩnh
+('Ga Hương Phố', '42', 18.3556, 105.6767),
+
+-- Quảng Bình
+('Ga Đồng Hới', '44', 17.4702, 106.6034),
+
+-- Thừa Thiên Huế
+('Ga Huế', '46', 16.4637, 107.5937),
+
+-- Đà Nẵng
+('Ga Đà Nẵng', '48', 16.0739, 108.2137),
+
+-- Quảng Ngãi
+('Ga Quảng Ngãi', '51', 15.1182, 108.7980),
+
+-- Khánh Hòa
+('Ga Nha Trang', '56', 12.2496, 109.1917),
+
+-- Thành phố Hồ Chí Minh
+('Ga Sài Gòn', '79', 10.7629, 106.6810),
+
+-- Đồng Nai
+('Ga Biên Hòa', '75', 10.9493, 106.8221),
+
+-- Bình Thuận
+('Ga Phan Thiết', '60', 10.9264, 108.1012),
+('Ga Bình Thuận', '60', 11.0975, 108.1225),
+
+-- Bình Định
+('Ga Quy Nhơn', '52', 13.7816, 109.2190);
+
+
+INSERT INTO seat_types (seat_type, `description`) 
+VALUES ('Ngồi mềm', 'Ghế ngồi mềm có điều hòa'), 
+       ('Giường nằm', 'Giường nằm điều hòa'),
+       ('Ngồi cứng', 'Ghế ngồi cứng có điều hòa');
+
+
+
+INSERT INTO passengers (full_name, passenger_type_id) 
+VALUES ('Nguyen Van A', 1), 
+       ('Nguyen Van B', 2), 
+       ('Nguyen Van C', 3);
+
+INSERT INTO routes (train_id, start_station_id, end_station_id, distance)
+VALUES (1, 1, 2, 120.5); -- Tuyến từ Ga Hà Nội (1) đến Ga Hải Phòng (2)
+
+
+       
+INSERT INTO train_cars (train_id, car_number, car_type, total_floors)
+VALUES 
+(1, '6', 'Giường nằm khoang 4', 28), 
+(1, '7', 'Giường nằm khoang 4', 28), 
+(1, '8', 'Giường nằm khoang 4', 28), 
+(1, '9', 'Giường nằm khoang 4', 28), 
+(1, '10', 'Giường nằm khoang 4', 28);
+
+INSERT INTO compartment (train_car_id, compartment_number, seat_type_id, capacity, floor)
+VALUES (1, 'Khoang 1', 1, 40, 1), 
+       (1, 'Khoang 2', 2, 20, 2);
+
+
+INSERT INTO seats (compartment_id, seat_number, price, `status`, floor) 
+VALUES (1, '1A', 100.00, 'Trống', 1), 
+       (1, '1B', 100.00, 'Trống', 1),
+       (2, '2A', 150.00, 'Trống', 2),
+       (2, '2B', 150.00, 'Trống', 2);
+
+INSERT INTO train_schedule (route_id, station_id, arrival_time, departure_time)
+VALUES (1, 1, NULL, '2024-09-15 08:00:00'), -- Khởi hành từ Ga Hà Nội
+       (1, 2, '2024-09-15 10:30:00', NULL); -- Đến Ga Hải Phòng
+
+
+INSERT INTO tickets (seat_id, passenger_id, price, booking_date, status) 
+VALUES (1, 1, 100.00, '2024-09-10 10:00:00', 'Đã đặt'), 
+       (2, 2, 50.00, '2024-09-10 11:00:00', 'Đã đặt');
