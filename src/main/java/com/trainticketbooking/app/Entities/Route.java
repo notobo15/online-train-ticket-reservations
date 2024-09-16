8bfb00cbaeb475471e1ca3b9293475ec46d09672
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-16 23:07:55 +0700

    modified: Route entity

--- src/main/java/com/trainticketbooking/app/Entities/Route.java
+++ src/main/java/com/trainticketbooking/app/Entities/Route.java
@@ -3,7 +3,9 @@ package com.trainticketbooking.app.Entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 @Entity
 @Table(name = "routes")
@@ -16,10 +18,6 @@ public class Route {
     private Long routeId;
 
     @ManyToOne
-    @JoinColumn(name = "train_id", nullable = false)
-    private Train train;
-
-    @ManyToOne
     @JoinColumn(name = "start_station_id", nullable = false)
     private Station startStation;
 
@@ -30,6 +28,22 @@ public class Route {
     @Column(name = "distance", nullable = false)
     private Double distance;
 
+    @Column(name = "arrival_time", nullable = false)
+    private LocalTime arrivalTime;
+
     @Column(name = "departure_time", nullable = false)
     private LocalTime departureTime;
+
+    @ManyToOne
+    @JoinColumn(name = "railway_route_id")
+    private RailwayRoute railwayRoute;
+
+    @Column(name = "station_number")
+    private int stationNumber;
+
+    @Column(name = "date_number")
+    private int dateNumber;
+
+    @OneToMany(mappedBy = "route")
+    private List<SeatRoute> seatRoutes;
 }
