914909352ee0b0072d005c377d44641b9c406cbe
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-17 23:50:04 +0700

    modified: removed: compartment and added status, compartmentNumber properties

--- src/main/java/com/trainticketbooking/app/Entities/Seat.java
+++ src/main/java/com/trainticketbooking/app/Entities/Seat.java
@@ -15,22 +15,29 @@ public class Seat {
     @Column(name = "seat_id")
     private Long seatId;
 
-    @ManyToOne
-    @JoinColumn(name = "compartment_id", nullable = false)
-    private Compartment compartment;
+    // @ManyToOne
+    // @JoinColumn(name = "compartment_id", nullable = true)
+    // private Compartment compartment;
 
-    @OneToMany(mappedBy = "seat")
-    private List<Ticket> tickets;
+    @ManyToOne
+    @JoinColumn(name = "carriage_id")
+    private Carriage carriage;
 
     @Column(name = "seat_number", nullable = false)
     private String seatNumber;
 
-    @Column(name = "price", nullable = false)
-    private Double price;
+    @OneToMany(mappedBy = "seat")
+    private List<Ticket> tickets;
 
-    @Column(name = "status", nullable = false)
-    private String status;
+    @ManyToOne
+    @JoinColumn(name = "seat_type_id", nullable = false)
+    private SeatType seatType;
 
-    @Column(name = "floor", nullable = false)
-    private int floor;
+    @Column(name = "floor")
+    private Integer floor;
+
+    @Column(name = "compartment_number", nullable = true)
+    private Integer compartmentNumber;
+
+    private boolean status;
 }
