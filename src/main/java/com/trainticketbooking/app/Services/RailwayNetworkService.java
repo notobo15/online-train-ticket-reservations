06e685f4aad77e1097f2a318c7834cd15379d564
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-18 20:08:59 +0700

    added: RailwayNetworkService service

---
+++ src/main/java/com/trainticketbooking/app/Services/RailwayNetworkService.java
@@ -0,0 +1,19 @@
+package com.trainticketbooking.app.Services;
+
+import com.trainticketbooking.app.Entities.RailwayNetwork;
+import com.trainticketbooking.app.Entities.RailwayRoute;
+import com.trainticketbooking.app.Entities.Train;
+import com.trainticketbooking.app.Repos.RailwayNetworkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class RailwayNetworkService {
+
+    @Autowired
+    private RailwayNetworkRepository railwayNetworkRepository;
+
+}
