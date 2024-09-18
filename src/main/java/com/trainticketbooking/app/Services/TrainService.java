f82ed353af5fc58f1ca4f2380d86f650ee137a1b
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-18 20:07:29 +0700

    added: TrainService service

---
+++ src/main/java/com/trainticketbooking/app/Services/TrainService.java
@@ -0,0 +1,32 @@
+package com.trainticketbooking.app.Services;
+
+import com.trainticketbooking.app.Entities.Carriage;
+import com.trainticketbooking.app.Entities.Seat;
+import com.trainticketbooking.app.Repos.CarriageRepository;
+import com.trainticketbooking.app.Repos.SeatRepository;
+import com.trainticketbooking.app.Repos.TrainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrainService {
+
+    @Autowired
+    private TrainRepository trainRepository;
+
+    @Autowired
+    private SeatRepository seatRepository;
+
+    @Autowired
+    private CarriageRepository carriageRepository;
+
+    public List<Seat> findSeatsByTrainId(Integer trainId) {
+        return seatRepository.findSeatsByTrainId(trainId);
+    }
+
+    public List<Carriage> findCarriagesByTrainId(Integer trainId) {
+        return carriageRepository.findByTrainTrainId(trainId);
+    }
+}
