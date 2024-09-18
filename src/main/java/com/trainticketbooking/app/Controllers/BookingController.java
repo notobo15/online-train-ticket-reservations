b13cd96f2bd9713a582428fb4574d560f7bc4d6c
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-18 20:15:18 +0700

    added: BookingController controllers

--- src/main/java/com/trainticketbooking/app/Controllers/BookingController.java
+++ src/main/java/com/trainticketbooking/app/Controllers/BookingController.java
@@ -2,16 +2,26 @@ package com.trainticketbooking.app.Controllers;
 
 import com.trainticketbooking.app.Entities.*;
 import com.trainticketbooking.app.Repos.*;
+import com.trainticketbooking.app.Services.RailwayNetworkService;
+import com.trainticketbooking.app.Services.RailwayRouteService;
+import com.trainticketbooking.app.Services.TrainService;
 import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+@Slf4j
 @Controller
 public class BookingController {
 
@@ -22,7 +32,7 @@ public class BookingController {
     private TrainScheduleRepository trainScheduleRepository;
 
     @Autowired
-    private TrainCarRepository trainCarRepository;
+    private CarriageRepository carriageRepository;
 
     @Autowired
     private SeatRepository seatRepository;
@@ -34,46 +44,64 @@ public class BookingController {
     private CompartmentRepository compartmentRepository;
 
     @Autowired
+    private TrainService trainService;
+
+    @Autowired
     private RouteRepository routeRepository;
+
+    @Autowired
+    private RailwayNetworkService railwayNetworkService;
+
+    @Autowired
+    private RailwayRouteService railwayRouteService;
 
     @GetMapping("/booking")
     public String showBookingForm(Model model) {
-        // Lấy tất cả các ga tàu (stations) để hiển thị trong dropdown form
         List<Station> stations = stationRepository.findAll();
         model.addAttribute("stations", stations);
         return "booking/booking-form";
     }
 
-    @GetMapping("/search-trains")
-    public String searchTrains(@RequestParam("startStation") Integer startStationId,
-                               @RequestParam("endStation") Integer endStationId,
-                               Model model) {
+    @PostMapping("/search")
+    public String searchTrains(
+            @RequestParam("departureStationId") Integer departureStationId,
+            @RequestParam("destinationStationId") Integer destinationStationId,
+            @RequestParam("departureDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate departureDate,
+            Model model) {
 
-        Station startStation = stationRepository.findById(startStationId).orElse(null);
-        Station endStation = stationRepository.findById(endStationId).orElse(null);
+        List<Train> availableTrains = railwayRouteService.findRailwayRouteWithStations(departureStationId,
+                destinationStationId, departureDate);
 
-        List<Route> routes = routeRepository.findByStartStationAndEndStation(startStation, endStation);
+        model.addAttribute("availableTrains", availableTrains);
+        return "booking/train-results";
+    }
 
-        model.addAttribute("routes", routes);
-        return "booking/train-list";
+    @GetMapping("/view-seats/{trainId}")
+    public String viewSeats(@PathVariable("trainId") Integer trainId, Model model) {
+        List<Seat> availableSeats = trainService.findSeatsByTrainId(trainId);
+        List<Carriage> carriages = trainService.findCarriagesByTrainId(trainId);
+
+        Map<Long, List<Integer>> floorNumbersMap = new HashMap<>();
+
+        for (Carriage carriage : carriages) {
+            List<Integer> floorNumbers = IntStream.rangeClosed(1, carriage.getTotalFloors())
+                    .boxed()
+                    .collect(Collectors.toList());
+            floorNumbersMap.put(carriage.getCarriageId(), floorNumbers);
+        }
+
+        model.addAttribute("availableSeats", availableSeats);
+        model.addAttribute("carriages", carriages);
+        model.addAttribute("floorNumbersMap", floorNumbersMap);
+
+        return "booking/seat-details";
     }
 
     @GetMapping("/train-details/{trainId}")
     public String showTrainDetails(@PathVariable("trainId") Long trainId, Model model) {
         Train train = trainRepository.findById(trainId).orElse(null);
-        List<TrainCar> trainCars = trainCarRepository.findByTrain(train);
-//        System.out.println(trainCars);
+        List<Carriage> trainCars = carriageRepository.findByTrain(train);
         model.addAttribute("trainCars", trainCars);
         return "booking/train-details";
-    }
-
-    @GetMapping("/compartment/{compartmentId}/seats")
-    public String showSeats(@PathVariable("compartmentId") Long compartmentId, Model model) {
-        // Lấy danh sách chỗ ngồi cho từng khoang
-        Compartment compartment = compartmentRepository.findById(compartmentId).orElse(null);
-        List<Seat> seats = seatRepository.findByCompartment(compartment);
-
-        model.addAttribute("seats", seats);
-        return "booking/seat-list";
     }
 }
