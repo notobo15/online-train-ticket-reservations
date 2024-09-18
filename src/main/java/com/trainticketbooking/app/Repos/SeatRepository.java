b8c665f42ec09c62f699d94ac4b12e7d072f3364
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-18 20:09:16 +0700

    added: SeatRepository repos

--- src/main/java/com/trainticketbooking/app/Repos/SeatRepository.java
+++ src/main/java/com/trainticketbooking/app/Repos/SeatRepository.java
@@ -3,11 +3,18 @@ package com.trainticketbooking.app.Repos;
 import com.trainticketbooking.app.Entities.Compartment;
 import com.trainticketbooking.app.Entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
-    List<Seat> findByCompartment(Compartment compartment);
+    List<Seat> findByCarriageCarriageId(Long carriageId);
+
+    List<Seat> findBySeatTypeSeatTypeId(Long seatTypeId);
+
+    @Query("SELECT s FROM Seat s WHERE s.carriage.train.trainId = :trainId")
+    List<Seat> findSeatsByTrainId(@Param("trainId") Integer trainId);
 }
