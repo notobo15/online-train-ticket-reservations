d781d5a101602d9fc0faca1c4cf9d1e230b7cd3f
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-28 00:06:10 +0700

    modified: extends IService<User> interface

--- src/main/java/com/trainticketbooking/app/Services/IUserService.java
+++ src/main/java/com/trainticketbooking/app/Services/IUserService.java
@@ -5,10 +5,6 @@ import com.trainticketbooking.app.Entities.User;
 import java.util.List;
 import java.util.Optional;
 
-public interface IUserService {
-    public List<User> getAllUsers();
-    public Optional<User> getUserById(Integer id);
-    public User saveUser(User user);
-    public void deleteUserById(Integer id);
+public interface IUserService extends IService<User> {
     public User findByUsername(String username);
 }
