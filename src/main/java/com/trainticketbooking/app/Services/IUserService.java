4369404381ce02b61885b8a64ff438fcd3aee478
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-28 23:21:17 +0700

    added: interface and impl IUserService

--- src/main/java/com/trainticketbooking/app/Services/IUserService.java
+++ src/main/java/com/trainticketbooking/app/Services/IUserService.java
@@ -2,9 +2,6 @@ package com.trainticketbooking.app.Services;
 
 import com.trainticketbooking.app.Entities.User;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface IUserService extends IService<User> {
     public User findByUsername(String username);
 }
