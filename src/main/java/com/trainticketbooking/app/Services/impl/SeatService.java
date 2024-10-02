0b53a27d748132219b054ddd72b9caf782f82771
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-10-02 22:59:07 +0700

    added: interface and impl Seats services

--- src/main/java/com/trainticketbooking/app/Services/impl/SeatService.java
+++ src/main/java/com/trainticketbooking/app/Services/impl/SeatService.java
@@ -1,21 +1,54 @@
 package com.trainticketbooking.app.Services.impl;
 
 import com.trainticketbooking.app.Dtos.SeatDto;
+import com.trainticketbooking.app.Entities.Seat;
 import com.trainticketbooking.app.Repos.SeatRepository;
+import com.trainticketbooking.app.Services.ISeatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class SeatService {
-
+public class SeatService implements ISeatService {
     @Autowired
     private SeatRepository seatRepository;
 
-    // Lấy danh sách ghế theo mã tàu
-    // public List<SeatDto> getSeats(Long trainId) {
-    // // Tìm kiếm ghế từ cơ sở dữ liệu
-    // return seatRepository.findByTrainId(trainId);
-    // }
+    @Override
+    public List<Seat> getAll() {
+        return seatRepository.findAll();
+    }
+
+    @Override
+    public Optional<Seat> getById(Integer id) {
+        return seatRepository.findById(id);
+    }
+
+    @Override
+    public Seat save(Seat seat) {
+        return seatRepository.save(seat);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        seatRepository.deleteById(id);
+    }
+
+    @Override
+    public Seat update(Seat seat) {
+        Optional<Seat> existingSeat = seatRepository.findById(seat.getSeatId());
+        if (existingSeat.isPresent()) {
+            Seat updatedSeat = existingSeat.get();
+            updatedSeat.setSeatNumber(seat.getSeatNumber());
+            updatedSeat.setCarriage(seat.getCarriage());
+            updatedSeat.setFloor(seat.getFloor());
+            return seatRepository.save(updatedSeat);
+        }
+        throw new RuntimeException("Seat not found with id: " + seat.getSeatId());
+    }
+
+    public List<Seat> findSeatsByCarriageId(Integer carriageId) {
+        return seatRepository.findByCarriageCarriageId(carriageId);
+    }
 }
