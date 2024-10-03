8419c0bf412061518a89693c5f851d4538309e41
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-10-03 23:39:41 +0700

    added: findAll method

--- src/main/java/com/trainticketbooking/app/Repos/TrainRepository.java
+++ src/main/java/com/trainticketbooking/app/Repos/TrainRepository.java
@@ -1,9 +1,13 @@
 package com.trainticketbooking.app.Repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.trainticketbooking.app.Entities.Train;
 
 @Repository
 public interface TrainRepository extends JpaRepository<Train, Integer> {
+    public Page<Train> findAll(Pageable pageable);
 }
