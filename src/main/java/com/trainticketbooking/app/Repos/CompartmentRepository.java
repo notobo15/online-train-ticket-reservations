a66f59521d1145e799ae9a5369a153575e20abf0
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-11 15:00:48 +0700

    removed: Compartment and TrainSchedule entities

--- src/main/java/com/trainticketbooking/app/Repos/CompartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trainticketbooking.app.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.trainticketbooking.app.Entities.Compartment;
-
-@Repository
-public interface CompartmentRepository extends JpaRepository<Compartment, Long> {
-}
