a52288f9d4da2970a12fb564d06685bf24694484
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-13 23:05:06 +0700

    added: findByEmail method of UserService service

--- src/main/java/com/trainticketbooking/app/Services/IUserService.java
+++ src/main/java/com/trainticketbooking/app/Services/IUserService.java
@@ -1,16 +1,13 @@
 package com.trainticketbooking.app.Services;
 
-import com.trainticketbooking.app.Entities.Train;
-import com.trainticketbooking.app.Entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
+import com.trainticketbooking.app.Entities.User;
 
 public interface IUserService extends IService<User> {
     public User findByUsername(String username);
-
+    public User findByEmail(String email);
     Page<User> findAll(Pageable pageable);
-
     public User adminUpdateUser(User user);
 }
